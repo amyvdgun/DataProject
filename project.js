1bbd83e68a429598b19b7731e5f16f644cc99d4f
Amy van der Gun <amyvdgun@live.nl>
2018-06-18 15:30:14 +0200

    commit van vandaag

--- project.js
+++ project.js
@@ -12,6 +12,7 @@
 window.onload = function () {
 
   var color = d3.scaleOrdinal(d3.schemeCategory10);
+
   // set the outer and inner width and height
   var margin = {top: 50, bottom: 50, left: 50, right: 50},
     width = 1100 - margin.left - margin.right,
@@ -66,6 +67,7 @@ window.onload = function () {
             scatterdata.push(alldata[firm].stats);
           }
 
+          // create scatter if all data has been loaded
           if (scatterdata.length > 500) {
             makeScatter(scatterdata);
           };
@@ -86,56 +88,48 @@ window.onload = function () {
           .range([height, 0])
           .domain(d3.extent(scatterdata, function(d) { return (d.returnOnEquity); })).nice();
 
-      // create axis objects
+      // create axes
       var xAxis = d3.axisBottom(x)
         .ticks(20, "s");
       var yAxis = d3.axisLeft(y)
         .ticks(20, "s");
-      // Draw Axis
-      var gX = scatterplot.append('g')
+
+      // set and draw the axes
+      var gX = scatterplot.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-      var gY = scatterplot.append('g')
-        //.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-        .call(yAxis);
+        .call(xAxis)
+        // .append("text")
+        //   .attr("class", "label")
+        //   .attr("x", width)
+        //   .attr("y", margin.bottom)
+        //   .style("text-anchor", "end")
+        //   .text("Beta");
 
-      //
-      // // // add x-axis
-      // var gX = scatterplot.append("g")
-      //     .attr("transform", "translate(0," + height + ")")
-      //     .call(xAxis)
-      //     .append("text")
-      //       .attr("class", "label")
-      //       .attr("x", width)
-      //       .attr("y", margin.bottom)
-      //       .style("text-anchor", "end")
-      //       .text("Beta");
-      //
-      // // add y-axis
-      // var gY = scatterplot.append("g")
-      //     .call(yAxis)
-      //     .append("text")
-      //       .attr("class", "label")
-      //       .attr("transform", "rotate(-90)")
-      //       .attr("x", 0)
-      //       .attr("y", -margin.left)
-      //       .attr("dy", ".71em")
-      //       .style("text-anchor", "end")
-      //       .text("Return on Equity");
+      var gY = scatterplot.append("g")
+        .call(yAxis)
+        // .append("text")
+        //   .attr("class", "label")
+        //   .attr("transform", "rotate(-90)")
+        //   .attr("x", 0)
+        //   .attr("y", -margin.left)
+        //   .attr("dy", ".71em")
+        //   .style("text-anchor", "end")
+        //   .text("Return on Equity");
 
-            // create a clipping region
+      // create a clipping region
       scatterplot.append("defs").append("clipPath")
           .attr("id", "clip")
         .append("rect")
           .attr("width", width)
           .attr("height", height);
-          
-        // Pan and zoom
+
+        // set zoom function
         var zoom = d3.zoom()
-          .scaleExtent([.5, 20])
+          .scaleExtent([1, 20])
           .extent([[0, 0], [width, height]])
           .on("zoom", zoomed);
 
+        // create rect for zoom option
         scatterplot.append("rect")
               .attr("width", width)
               .attr("height", height)
